921e35c401b80b69cbac7cacee9ff3846450311a
thawzzin <thawzzin.16@gmail.com>
2025-12-16 19:41:37 +0700

    feat: events page responsive

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -4,8 +4,10 @@ import { upcomingShows } from "@/constants/event";
 const Events = () => {
   return (
     <div className="p-5 overflow-hidden">
-      <h1 className="text-[18.9vw] font-medium mb-5 leading-[0.8]">Upcoming shows</h1>
-      <div className="divide-y divide-secondary/50 border-t border-b border-secondary/50">
+      <h1 className="text-[120px] md:text-[18.9vw] font-medium mb-5 leading-[0.8]">
+        Upcoming shows
+      </h1>
+      <div className="md:divide-y divide-secondary/50 md:border-t md:border-b border-secondary/50">
         {upcomingShows.map((event, index) => (
           <UpcomingCards key={index} event={event} />
         ))}
